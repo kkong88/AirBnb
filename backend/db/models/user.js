5a9b314d064f58c02f277de84682d0b0bf357548
Kelly Kong <kkong1992@gmail.com>
2023-01-11 19:23:11 -0500

    updated user.js

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -37,8 +37,23 @@ module.exports = (sequelize, DataTypes) => {
           len: [60, 60]
         }
       }
-    }, {
-      sequelize:sequelize
+    },
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+        }
+      },
+      scopes: {
+        currentUser: {
+          attributes: { exclude: ["hashedPassword"] }
+        },
+        loginUser: {
+          attributes: {}
+        }
+      }
     }
   );
   return User;
