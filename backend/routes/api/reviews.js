f010bfb9313b281c2f061a9205bbde64507778e8
Kelly Kong <kkong1992@gmail.com>
2023-01-25 18:43:19 -0500

    finished review id images route

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -13,6 +13,10 @@ const review = require('../../db/models/review');
 const spotimage = require('../../db/models/spotimage');
 const router = express.Router();
 
+const validateImage = [
+    check()
+]
+
 router.get('/current', requireAuth, async (req, res)=> {
     const reviews = await Review.findAll({
          where: { userId: req.user.id},
@@ -40,6 +44,45 @@ router.get('/current', requireAuth, async (req, res)=> {
     res.json({reviews})
 })
 
+// router.post('/:id/images', requireAuth, async (req, res)=> {
+//     const { url } = req.body
+//     let image = await Review.findByPk(req.params.id)
+//      image = await reviewImage.create(
+//         {
+//             url: url
+//         })
+//         if (image.length > 10){
+//             res.status(403).json({message: 'Maximum number of image for this reached', statusCode: 403})
+//         }
+//         if(!image){
+//             res.status(404).json({message: 'Review couldnt be found', statusCode: 404})
+//         }
+//     res.json(image)
+// })
 
+router.post('/:id/images', requireAuth, async (req, res) => {
+    const { url } = req.body
+    const review = await Review.findByPk(req.params.id)
+    const imageReview = await reviewImage.findAll({
+        where: {
+            reviewId: review.id
+        }
+    })
+    if(!review){
+        res.status(404).json({message: 'Review couldnt be found'})
+    } else if (imageReview.length >= 10){
+        res.status(403).json({message: 'Maximum number of image for this resource was reached'})
+    } else {
+        const image = await reviewImage.create({
+            reviewId : review.id,
+            url: url
+        })
+        const obj = {
+            id: image.id,
+            url: image.url
+        }
+        res.json(obj)
+    }
+})
 
 module.exports = router;
