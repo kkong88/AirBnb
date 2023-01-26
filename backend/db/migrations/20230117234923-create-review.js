668b6317250cc049ccffa98482f846851323c825
Kelly Kong <kkong1992@gmail.com>
2023-01-26 18:18:18 -0500

    updated options in migration

--- backend/db/migrations/20230117234923-create-review.js
+++ backend/db/migrations/20230117234923-create-review.js
@@ -44,7 +44,9 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    },
+    options
+    );
   },
   down: async(queryInterface, Sequelize) => {
     options.tableName = "Reviews"
--- backend/db/migrations/20230123231735-create-review-image.js
+++ backend/db/migrations/20230123231735-create-review-image.js
@@ -32,7 +32,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    },options);
   },
   down: async (queryInterface, Sequelize) => {
     options.tableName = 'reviewImages'
