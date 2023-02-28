fcbfb4310e59aa3786716c0d48ec917b4372bb86
Kelly Kong <kkong1992@gmail.com>
2023-02-27 20:51:33 -0500

    added preview image

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -5,15 +5,23 @@ import { getSpot } from '../../store/spot'
 
 function LandingPage(){
     const dispatch = useDispatch()
-    const spots = useSelector((state) => state.spot)
+    const spots = useSelector((state) => state?.spot)
 
     useEffect(() => {
         dispatch(getSpot())
     },[dispatch])
+
+    const previewImage = spots?.Spot
     
     return (
-        <div>
-        </div>
+    <div>
+        {previewImage?.map(spot => (
+             <div key={spot.id}>
+                 <img src={spot.imageUrl} alt={spot.name} />
+            </div>
+        ))}
+
+    </div>
     )
 }
 
