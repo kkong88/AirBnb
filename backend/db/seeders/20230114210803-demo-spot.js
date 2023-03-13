baebe05b83e235b44564b7afae650185e947399b
Kelly Kong <kkong1992@gmail.com>
2023-03-12 21:30:36 -0400

    updated seeders

--- backend/db/seeders/20230114210803-demo-spot.js
+++ backend/db/seeders/20230114210803-demo-spot.js
@@ -19,10 +19,34 @@ module.exports = {
       lat: 8.88,
       lng: 1.23,
       name: 'Luxury apt',
-      price: 100.00,
+      price: 1000.00,
       // avgRating: 4.6,
       // previewImage: "test.url",
       description: 'Check out this great place to have a unique place to stay'
+    },
+    {
+      ownerId: 1,
+      address: '5th street',
+      city:'New York',
+      state:'NY',
+      country: "United states",
+      lat: 8.88,
+      lng: 1.23,
+      name: 'Luxury Beautiful riverfront mid-century house',
+      price: 790.00,
+      description: 'We are conveniently located minutes from downtown Newburgh, and just over the bridge from the Beacon train station. 90 mins by train, or about a one-hour drive from NYC. The house is within minutes of the best the Hudson Valley has to offer, including Storm King (15min), Dia Beacon (15min), great hiking trails, local farms and distilleries, Woodbury Commons (25min), Mohonk Preserve (30min) and more.'
+    },
+    {
+      ownerId: 1,
+      address: '69 blackberry lane',
+      city:'Boston',
+      state:'Ma',
+      country: "United states",
+      lat: 8.88,
+      lng: 1.23,
+      name: 'Cozy Home',
+      price: 160.00,
+      description: "Relax & Rejuvenate w/ panoramic views of White Mountains from any of the 4 decks of Kailaśa Chalet! Perched on top of a mountain overlooking Mt Chocorua & Silver Lake w/ majestic views of Mt Washington Valley. So easy to get lost in the beauty of Kailaśa!"
     },
     {
       ownerId: 2,
@@ -33,10 +57,32 @@ module.exports = {
       lat: 12.12,
       lng: 13.13,
       name: 'Back Bay',
-      price: 10.99,
-      // avgRating: 4.8,
-      // previewImage: "test.url",
+      price: 500,
       description: 'Come checkout beautiful back bay with shops and good eats around'
+    },
+    {
+      ownerId: 2,
+      address: '17 cottage lane',
+      city:'York',
+      state:'Me',
+      country: "United states",
+      lat: 12.12,
+      lng: 13.13,
+      name: 'Paradise in York',
+      price: 690,
+      description: 'This vast 6 bedroom property offers you everything you need for your dream vacation: Superb 180 degree ocean view, 3 terrasses, grill, heated pool, hot tub, pool and air hockey tables, fire pit, interior fireplace, fenced area for dogs, 9 tvs, 2 sets of dishwashers, 2 sets of washer/dryer, Keurig machine on every floor, main floor adapted for wheelchairs, etc.'
+    },
+    {
+      ownerId: 2,
+      address: '3A rainbow rd',
+      city:'Braintree',
+      state:'Ma',
+      country: "United states",
+      lat: 12.12,
+      lng: 13.13,
+      name: 'Rental unit',
+      price: 320,
+      description: 'Amazing beautiful apartment with gorgeous sunset view.  A two minute drive to expressway.'
     },
     {
       ownerId: 3,
@@ -47,11 +93,33 @@ module.exports = {
       lat: 18.18,
       lng: 19.23,
       name: 'Newbury place',
-      price: 16.00,
-      // avgRating: 4.2,
-      // previewImage: "test.url",
+      price: 750.00,
       description: 'A place to remember your stay'
-    }
+    },
+    {
+      ownerId: 3,
+      address: '100 universal city plaze',
+      city:'hogsmeade',
+      state:'England',
+      country: "United Kingdom",
+      lat: 18.18,
+      lng: 19.23,
+      name: 'Hogwarts',
+      price: 12000.00,
+      description: 'Avada Kedavra'
+    },
+    {
+      ownerId: 3,
+      address: 'Unknown location',
+      city:'space',
+      state:'Galaxy',
+      country: "N/A",
+      lat: 18.18,
+      lng: 19.23,
+      name: 'Death Star',
+      price: 10000.00,
+      description: 'A once in a lifetime experiance'
+    },
   ],{})
     /**
      * Add seed commands here.
--- backend/db/seeders/20230120003503-demo-spotImages.js
+++ backend/db/seeders/20230120003503-demo-spotImages.js
@@ -15,23 +15,43 @@ module.exports = {
       preview: true
     },
     {
-      spotId: 1,
+      spotId: 2,
       url: 'https://res.cloudinary.com/dip4w3xmy/image/upload/v1678048032/APARTMENTS-MAIN_i_xv0b5f.jpg',
       preview: true
     },
     {
-    spotId: 2,
+    spotId: 3,
     url: 'https://res.cloudinary.com/dip4w3xmy/image/upload/v1678048085/boston-neighborhoods-guide_ll5te9.jpg',
     preview: true
     },
     {
-      spotId: 2,
+      spotId: 4,
       url: 'https://res.cloudinary.com/dip4w3xmy/image/upload/v1678048121/Newbury-Street-480x270_z0zcjp.jpg',
       preview: true
     },
     {
-      spotId: 3,
+      spotId: 5,
+      url: 'https://res.cloudinary.com/dip4w3xmy/image/upload/v1678670700/1-city-point_atnaru.webp',
+      preview: true
+    },
+    {
+      spotId: 6,
       url: 'https://res.cloudinary.com/dip4w3xmy/image/upload/v1678298719/RH-3509Lanewood-1098__e0tycr.jpg',
+      preview: true
+    },
+    {
+      spotId: 7,
+      url: 'https://res.cloudinary.com/dip4w3xmy/image/upload/v1678670700/maxresdefault_knyhzq.jpg',
+      preview: true
+    },
+      {
+      spotId: 8,
+      url: 'https://res.cloudinary.com/dip4w3xmy/image/upload/v1678670025/Hogwarts-Castle_rbktae.jpg',
+      preview: true
+    },
+    {
+      spotId: 9,
+      url: 'https://res.cloudinary.com/dip4w3xmy/image/upload/v1678670113/Death_star1_lemgqh.png',
       preview: true
     }
   ])
